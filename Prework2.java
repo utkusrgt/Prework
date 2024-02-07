cadb92544a30a22e49e00083c3077f61950bbbfd
Utku Sürgit <usurgit@gmail.com>
2024-02-07 21:01:05 +0300

    Prework 2 tamamlama

---
+++ Prework2.java
@@ -0,0 +1,6 @@
+public class Prework2 {
+    public static void main(String[] args) {
+        String x = "Patika.dev FullStack Java Spring boot & React Bootcamp";
+        System.out.println(x);
+    }
+}
