33b0a0851e215763bf54e78c19a5a9c499ba1df7
Utku Sürgit <usurgit@gmail.com>
2024-02-14 15:22:48 +0300

    Cohort'ta belirtilen açıklamaya uygun hale getirildi.

--- Prework2.java
+++ Prework2.java
@@ -1,6 +1,5 @@
 public class Prework2 {
     public static void main(String[] args) {
-        String x = "Patika.dev FullStack Java Spring boot & React Bootcamp";
-        System.out.println(x);
+        System.out.println("Hello, World!");
     }
 }
